3c4ded9eef34eafe416204a73788ac6a5144167c
benstoltz <stoltz.ben@me.com>
2015-03-11 13:07:30 -0400

    Twitter Search Script
    inserting data into object and into MongoDB
    
    Signed-off-by: benstoltz <stoltz.ben@me.com>

--- twitterSearchExample.py
+++ twitterSearchExample.py
@@ -4,6 +4,7 @@ from urllib2 import URLError
 from httplib import BadStatusLine
 import twitter
 import pymongo
+import json
 
 
 def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
@@ -44,7 +45,8 @@ def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
 
     while True:
         try:
-            return twitter_api_func(*args, **kw)
+            print "frank"
+            return twitter_api_func
         except twitter.api.TwitterHTTPError, e:
             error_count = 0
             wait_period = handle_twitter_http_error(e, wait_period)
@@ -64,11 +66,9 @@ def make_twitter_request(twitter_api_func, max_errors=10, *args, **kw):
                 raise
 
 
-def save_to_mongo(data, mongo_db, mongo_db_collection, **mongo_conn_kw):
+def save_to_mongo(data, mongo_db, mongo_db_collection):
     # Connects to the MongoDB server running on
     # localhost: 27017 by default
-
-    client = pymongo.MongoClient(**mongo_conn_kw)
 
     # Get a reference to a particular database
 
@@ -109,12 +109,13 @@ def twitter_search(twitter_api, q, max_results=200, **kw):
     # Reasonable number of results is ~1000, although not all queries will return that number
 
     # Enforce limit
-    max_results = min(10000, max_results)
+    max_results = min(100000, max_results)
 
-    for _ in range(10):  # 10 * 100 = 1000
+    for _ in range(1000):  # 10 * 100 = 1000
         try:
             next_results = search_results['search_metadata']['next_results']
         except KeyError, e:  # No more results when next_results doesn't exist
+            print "Done here!"
             break
 
         # Create a dictionary from next_results, which has the following form:
@@ -128,8 +129,71 @@ def twitter_search(twitter_api, q, max_results=200, **kw):
             break
     return statuses
 
+# Open mongoDB connection:
+
+client = pymongo.MongoClient()
+
+# Set up Oauth login
 twitter_api = oauth_login()
 
+# Create query
 q = 'chikungunya'
+# qtest = 'israel'
 
-response = twitter_search(twitter_api, q, max_results=10000)
+# Create response
+responses = twitter_search(twitter_api, q, max_results=100000)
+
+for response in responses:
+    default = 'Null'
+    tweet = {}
+    tweet['text'] = response.get('text', default)
+    tweet['in_reply_to_status_id'] = response.get('in_reply_to_status', default)
+    tweet['id'] = response.get('id', default)
+    tweet['favorite_count'] = response.get('favorite_count', default)
+    tweet['in_reply_to_screen_name'] = response.get('in_reply_to_screen_name', default)
+    tweet['in_reply_to_user_id'] = response.get('in_reply_to_user_id', default)
+    tweet['retweet_count'] = response.get('retweet_count', default)
+    tweet['favorited'] = response.get('favorited', default)
+    # Start user object
+    tweet['user'] = {}
+    user = response.get('user', default)
+    tweet['user']['id'] = user.get('id', default)
+    tweet['user']['followers_count'] = user.get('followers_count', default)
+    tweet['user']['listed_count'] = user.get('listed_count', default)
+    tweet['user']['is_translation_enabled'] = user.get('is_translation_enabled', default)
+    tweet['user']['utc_offset'] = user.get('utc_offset', default)
+    tweet['user']['statuses_count'] = user.get('statuses_count', default)
+    tweet['user']['description'] = user.get('description', default)
+    tweet['user']['friends_count'] = user.get('friends_count', default)
+    tweet['user']['location'] = user.get('location', default)
+    tweet['user']['following'] = user.get('following', default)
+    tweet['user']['geo_enabled'] = user.get('geo_enabled', default)
+    tweet['user']['name'] = user.get('name', default)
+    tweet['user']['lang'] = user.get('lang', default)
+    tweet['user']['url'] = user.get('url', default)
+    tweet['user']['account_created'] = user.get('created_at', default)
+    tweet['user']['contributors_enabled'] = user.get('contributors_enabled', default)
+    tweet['user']['timezone'] = user.get('timezone', default)
+    tweet['user']['protected'] = user.get('protected', default)
+    tweet['user']['default_profile'] = user.get('default_profile', default)
+    tweet['user']['is_translator'] = user.get('is_translator', default)
+    # End user
+    # placeholder for geo
+    tweet['possibly_sensitive'] = response.get('possibly_sensitive', default)
+    tweet['lang'] = response.get('lang', default)
+    tweet['tweet_created_at'] = response.get('created_at', default)
+    # Start metadata
+    tweet['metadata'] = {}
+    tweet['metadata']['iso_language_code'] = response['metadata']['iso_language_code']
+    tweet['metadata']['result_type'] = response['metadata']['result_type']
+
+    if response['geo'] and response['geo']['type'] == 'Point':
+        tweet['geo'] = response.get('geo', default)
+        tweet['place'] = response.get('place', default)
+        # print json.dumps(tweet, indent=1)
+        save_to_mongo(tweet, 'chikungunyaDatabase', 'geolocated')
+        # print "bob"
+    else:
+        # print json.dumps(tweet, indent=1)
+        save_to_mongo(tweet, 'chikungunyaDatabase', 'tweets')
+        # print "bob"
